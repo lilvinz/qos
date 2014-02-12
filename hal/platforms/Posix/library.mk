95d037ce888dc73ac6bed1dfd352a02ae12ce4fe
vke <vincent.kessler@quantec-networks.de>
2014-02-12 19:44:37 +0100

    qos posix: added linker script which supports module_init

--- hal/platforms/Posix/library.mk
+++ hal/platforms/Posix/library.mk
@@ -4,6 +4,8 @@ QHAL_POSIX_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 CSRC += $(wildcard $(QHAL_POSIX_DIR)/*.c)
 EXTRAINCDIRS += $(QHAL_POSIX_DIR)
 
+LDFLAGS += -T$(QHAL_POSIX_DIR)/ld/sections.ld
+
 # ChibiOS
 include $(ROOT_DIR)/src/lib/ChibiOS/os/hal/platforms/Posix/platform.mk
 include $(QHAL_POSIX_DIR)/../../../ports/GCC/SIMIA32/port.mk
