06cd39ba1c04e63e78da37b4158eb2499db17c78
vke <vincent.kessler@quantec-networks.de>
2014-03-03 19:24:29 +0100

    qos: added STM32F1xx platform

---
+++ hal/platforms/STM32F1xx/library.mk
@@ -0,0 +1,21 @@
+
+QHAL_STM32F1XX_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+CSRC += $(wildcard $(QHAL_STM32F1XX_DIR)/*.c)
+CSRC += $(wildcard $(QHAL_STM32F1XX_DIR)/../STM32/FLASHv1/*.c)
+CSRC += $(wildcard $(QHAL_STM32F1XX_DIR)/../STM32/WDGv1/*.c)
+CSRC += $(wildcard $(QHAL_STM32F1XX_DIR)/../STM32/USARTv1/*.c)
+
+EXTRAINCDIRS += $(QHAL_STM32F1XX_DIR)
+EXTRAINCDIRS += $(QHAL_STM32F1XX_DIR)/../STM32/FLASHv1
+EXTRAINCDIRS += $(QHAL_STM32F1XX_DIR)/../STM32/WDGv1
+EXTRAINCDIRS += $(QHAL_STM32F1XX_DIR)/../STM32/USARTv1
+
+LDFLAGS += -T$(QHAL_STM32F1XX_DIR)/ld/sections.ld
+
+# ChibiOS hal
+include $(ROOT_DIR)/src/lib/ChibiOS/os/hal/platforms/STM32F1xx/platform.mk
+# Chibios port
+include $(ROOT_DIR)/src/lib/ChibiOS/os/ports/GCC/ARMCMx/STM32F1xx/port.mk
+
+include $(QHAL_STM32F1XX_DIR)/../STM32/library.mk
---
+++ hal/platforms/STM32F1xx/qhal_lld.h
@@ -0,0 +1,88 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    STM32F1xx/qhal_lld.h
+ * @brief   STM32F1xx HAL subsystem low level driver header.
+ * @pre     This module requires the following macros to be defined in the
+ *          @p board.h file:
+ *          - STM32_LSECLK.
+ *          - STM32_HSECLK.
+ *          - STM32_HSE_BYPASS (optionally).
+ *          .
+ *          One of the following macros must also be defined:
+ *          - STM32F10X_LD_VL for Value Line Low Density devices.
+ *          - STM32F10X_MD_VL for Value Line Medium Density devices.
+ *          - STM32F10X_LD for Performance Low Density devices.
+ *          - STM32F10X_MD for Performance Medium Density devices.
+ *          - STM32F10X_HD for Performance High Density devices.
+ *          - STM32F10X_XL for Performance eXtra Density devices.
+ *          - STM32F10X_CL for Connectivity Line devices.
+ *          .
+ *
+ * @addtogroup QHAL
+ * @{
+ */
+
+#ifndef _QHAL_LLD_H_
+#define _QHAL_LLD_H_
+
+#include "stm32.h"
+
+/*===========================================================================*/
+/* Driver constants.                                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Platform capabilities.                                                    */
+/*===========================================================================*/
+
+/**
+ * @name    STM32F1xx capabilities
+ * @{
+ */
+
+/* FLASH attributes.*/
+#define STM32_HAS_FLASH         TRUE
+
+/** @} */
+
+/*===========================================================================*/
+/* Driver pre-compile time settings.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver data structures and types.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver macros.                                                            */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+/* STM32 ISR, DMA and RCC helpers.*/
+#include "qstm32_isr.h"
+
+#endif /* _QHAL_LLD_H_ */
+
+/** @} */
