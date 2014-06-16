9a29b38f7560e2fb5db33ee4ff9bb0d8d42520fc
vke <vincent.kessler@quantec-networks.de>
2014-06-16 18:15:43 +0200

    nvm_tools: added support for write_alignment

--- src/nvm_tools.c
+++ src/nvm_tools.c
@@ -52,12 +52,30 @@ int nvmcmp(BaseNVMDevice* devap, BaseNVMDevice* devbp, uint32_t n)
  */
 bool nvmcpy(BaseNVMDevice* dstp, BaseNVMDevice* srcp, uint32_t n)
 {
-    for (uint32_t i = 0; i < n; ++i)
+    NVMDeviceInfo di;
+
+    if (nvmGetInfo(dstp, &di) != CH_SUCCESS)
+        return false;
+
+    chDbgAssert(di.write_alignment <= 4, "nvmcpy(), #1",
+            "unsupported write_alignment");
+
+    if (di.write_alignment == 0)
+        di.write_alignment = 1;
+
+    for (uint32_t i = 0; i < n; i += di.write_alignment)
     {
-        uint8_t byte;
-        if (nvmRead(srcp, i, 1, &byte) != CH_SUCCESS)
+        uint8_t temp[4] = { 0xff, 0xff, 0xff, 0xff };
+
+        uint8_t chunk_n = di.write_alignment;
+        if (n - i < di.write_alignment)
+            chunk_n = n - i;
+
+        if (nvmRead(srcp, i, chunk_n, temp) != CH_SUCCESS)
             return false;
-        if (nvmWrite(dstp, i, 1, &byte) != CH_SUCCESS)
+
+        /* Note: Possibly remaining bytes are filled with 0xff. */
+        if (nvmWrite(dstp, i, di.write_alignment, temp) != CH_SUCCESS)
             return false;
     }
     return true;
@@ -66,7 +84,7 @@ bool nvmcpy(BaseNVMDevice* dstp, BaseNVMDevice* srcp, uint32_t n)
 /**
  * @brief   Sets data of a BaseNVMDevice object to a desired pattern.
  *
- * @param[in] dstp      pointer to the first @p BaseNVMDevice object
+ * @param[in] dstp      pointer to a @p BaseNVMDevice object
  * @param[in] pattern   pattern to set memory to
  * @param[in] n         number of bytes to set
  *
@@ -78,9 +96,19 @@ bool nvmcpy(BaseNVMDevice* dstp, BaseNVMDevice* srcp, uint32_t n)
  */
 bool nvmset(BaseNVMDevice* dstp, uint8_t pattern, uint32_t n)
 {
-    for (uint32_t i = 0; i < n; ++i)
+    NVMDeviceInfo di;
+
+    if (nvmGetInfo(dstp, &di) == CH_SUCCESS)
+        return false;
+
+    chDbgAssert(di.write_alignment <= 4, "nvmset(), #1",
+            "unsupported write_alignment");
+
+    const uint8_t temp[4] = { pattern, pattern, pattern, pattern };
+
+    for (uint32_t i = 0; i < n; i += di.write_alignment)
     {
-        if (nvmWrite(dstp, i, 1, &pattern) != CH_SUCCESS)
+        if (nvmWrite(dstp, i, di.write_alignment, temp) != CH_SUCCESS)
             return false;
     }
     return true;
