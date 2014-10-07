0a9bfabec02acbc0d2c6e5eac2c7b554976949b0
vke <vincent.kessler@quantec-networks.de>
2014-10-07 22:33:32 +0200

    platforms/AT91SAM7X: rename to AT91SAM7 for consistency with ChibiOS

---
+++ hal/platforms/AT91SAM7/library.mk
@@ -0,0 +1,17 @@
+
+# ChibiOS default path if not specified
+CHIBIOS_DIR ?= $(ROOT_DIR)/submodules/chibios
+CHIBIOS := $(CHIBIOS_DIR)
+
+QHAL_AT91SAM7_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+CSRC += $(wildcard $(QHAL_AT91SAM7_DIR)/*.c)
+EXTRAINCDIRS += $(QHAL_AT91SAM7_DIR)
+
+LDFLAGS += -T$(QHAL_AT91SAM7_DIR)/ld/sections.ld
+
+# ChibiOS
+include $(CHIBIOS_DIR)/os/hal/platforms/AT91SAM7/platform.mk
+include $(CHIBIOS_DIR)/os/ports/GCC/ARM/AT91SAM7/port.mk
+
+include $(QHAL_AT91SAM7_DIR)/../../library.mk
