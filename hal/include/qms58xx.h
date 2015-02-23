a33469887e31641f88a730343ee4c29d9b2aa2ce
vke <vincent.kessler@quantec-networks.de>
2015-02-23 21:27:03 +0100

    qms58xx: add driver

---
+++ hal/include/qms58xx.h
@@ -0,0 +1,161 @@
+/**
+ * @file    qms58xx.h
+ * @brief   MS58XX driver header.
+ *
+ * @addtogroup MS58XX
+ * @{
+ */
+
+#ifndef _QMS58XX_H_
+#define _QMS58XX_H_
+
+#if HAL_USE_MS58XX || defined(__DOXYGEN__)
+
+/*===========================================================================*/
+/* Driver constants.                                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver pre-compile time settings.                                         */
+/*===========================================================================*/
+
+/**
+ * @name    MS58XX configuration options
+ * @{
+ */
+
+/**
+ * @brief   Enables the @p ms58xxAcquireBus() and @p ms58xxReleaseBus() APIs.
+ * @note    Disabling this option saves both code and data space.
+ */
+#if !defined(MS58XX_USE_MUTUAL_EXCLUSION) || defined(__DOXYGEN__)
+#define MS58XX_USE_MUTUAL_EXCLUSION     FALSE
+#endif
+/** @} */
+
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+#if !HAL_USE_I2C
+#error "MS58XX driver requires HAL_USE_I2C"
+#endif
+
+#if MS58XX_USE_MUTUAL_EXCLUSION && !CH_USE_MUTEXES && !CH_USE_SEMAPHORES
+#error "MS58XX_USE_MUTUAL_EXCLUSION requires CH_USE_MUTEXES "
+       "and/or CH_USE_SEMAPHORES"
+#endif
+
+/*===========================================================================*/
+/* Driver data structures and types.                                         */
+/*===========================================================================*/
+
+/**
+ * @brief   Possible values for api oversampling argument.
+ */
+enum ms58xx_osr_e
+{
+    MS58XX_OSR_256 = 0,
+    MS58XX_OSR_512 = 2,
+    MS58XX_OSR_1024 = 4,
+    MS58XX_OSR_2048 = 6,
+    MS58XX_OSR_4096 = 8,
+};
+
+/**
+ * @brief   Driver state machine possible states.
+ */
+typedef enum
+{
+    MS58XX_UNINIT = 0,              /**< Not initialized.                   */
+    MS58XX_STOP = 1,                /**< Stopped.                           */
+    MS58XX_READY = 3,               /**< Device ready.                      */
+    MS58XX_ACTIVE= 4,               /**< Device converting.                 */
+} ms58xxstate_t;
+
+/**
+ * @brief   Type of a structure representing an I2C driver.
+ */
+typedef struct MS58XXDriver MS58XXDriver;
+
+/**
+ * @brief   MS58XX over I2C driver configuration structure.
+ */
+typedef struct
+{
+    /**
+     * @brief I2C driver associated to this MS58XX driver.
+     */
+    I2CDriver* i2cp;
+    uint8_t i2c_address;
+    systime_t i2c_timeout;
+} MS58XXConfig;
+
+/**
+ * @extends BaseNVMDevice
+ *
+ * @brief   Structure representing a MS58XX driver.
+ */
+struct MS58XXDriver
+{
+    /**
+    * @brief Driver state.
+    */
+    ms58xxstate_t state;
+    /**
+    * @brief Current configuration data.
+    */
+    const MS58XXConfig* configp;
+#if MS58XX_USE_MUTUAL_EXCLUSION || defined(__DOXYGEN__)
+#if CH_USE_MUTEXES || defined(__DOXYGEN__)
+    /**
+     * @brief Mutex protecting the device.
+     */
+    Mutex mutex;
+#elif CH_USE_SEMAPHORES
+    Semaphore semaphore;
+#endif
+#endif /* MS58XX_USE_MUTUAL_EXCLUSION */
+    /**
+    * @brief Calibration data read from chip rom.
+    */
+    uint16_t calibration[8];
+    /**
+    * @brief Cached last raw readings
+    */
+    uint32_t last_d1;
+    uint32_t last_d2;
+};
+
+/*===========================================================================*/
+/* Driver macros.                                                            */
+/*===========================================================================*/
+
+/** @} */
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    void ms58xxInit(void);
+    void ms58xxObjectInit(MS58XXDriver* ms58xxp);
+    void ms58xxStart(MS58XXDriver* ms58xxp, const MS58XXConfig* configp);
+    void ms58xxStop(MS58XXDriver* ms58xxp);
+    void ms58xxAcquireBus(MS58XXDriver* ms58xxp);
+    void ms58xxReleaseBus(MS58XXDriver* ms58xxp);
+    bool_t ms58xxTemperatureStart(MS58XXDriver* ms58xxp, enum ms58xx_osr_e osr);
+    bool_t ms58xxTemperatureResult(MS58XXDriver* ms58xxp, float *resultp);
+    bool_t ms58xxPressureStart(MS58XXDriver* ms58xxp, enum ms58xx_osr_e osr);
+    bool_t ms58xxPressureResult(MS58XXDriver* ms58xxp, float *resultp);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HAL_USE_MS58XX */
+
+#endif /* _QMS58XX_H_ */
+
+/** @} */
---
+++ hal/src/qms58xx.c
@@ -0,0 +1,520 @@
+/**
+ * @file    qms58xx.c
+ * @brief   MS58XX driver code.
+ *
+ * @addtogroup MS58XX
+ * @{
+ */
+
+#include "ch.h"
+#include "qhal.h"
+
+#if HAL_USE_MS58XX || defined(__DOXYGEN__)
+
+#include <string.h>
+/*
+ * @todo    - add sanity check for calibration values
+ *          - add conversion error detection and recovery
+ */
+
+/*===========================================================================*/
+/* Driver local definitions.                                                 */
+/*===========================================================================*/
+
+enum command_e
+{
+    MS58XX_COMMAND_RESET = 0x1e,
+    MS58XX_COMMAND_ACQUIRE_D1 = 0x40,
+    MS58XX_COMMAND_ACQUIRE_D2 = 0x50,
+    MS58XX_COMMAND_READ_ADC = 0x00,
+    MS58XX_COMMAND_READ_PROM = 0xa0,
+};
+
+enum
+{
+    MS58XX_CAL_0 = 0,
+    MS58XX_CAL_RESERVED = 0,
+    MS58XX_CAL_1 = 1,
+    MS58XX_CAL_SENST1 = 1,
+    MS58XX_CAL_2 = 2,
+    MS58XX_CAL_OFFT1 = 2,
+    MS58XX_CAL_3 = 3,
+    MS58XX_CAL_TCS = 3,
+    MS58XX_CAL_4 = 4,
+    MS58XX_CAL_TCO = 4,
+    MS58XX_CAL_5 = 5,
+    MS58XX_CAL_TREF = 5,
+    MS58XX_CAL_6 = 6,
+    MS58XX_CAL_TEMPSENS = 6,
+    MS58XX_CAL_7 = 7,
+    MS58XX_CAL_CRC = 7,
+};
+
+/*===========================================================================*/
+/* Driver exported variables.                                                */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local variables and types.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local functions.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver exported functions.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   MS58XX driver initialization.
+ * @note    This function is implicitly invoked by @p halInit(), there is
+ *          no need to explicitly initialize the driver.
+ *
+ * @init
+ */
+void ms58xxInit(void)
+{
+}
+
+/**
+ * @brief   Initializes an instance.
+ *
+ * @param[out] ms58xxp  pointer to the @p MS58XXDriver object
+ *
+ * @init
+ */
+void ms58xxObjectInit(MS58XXDriver* ms58xxp)
+{
+    ms58xxp->state = MS58XX_STOP;
+    ms58xxp->configp = NULL;
+    ms58xxp->last_d1 = 0;
+    ms58xxp->last_d2 = 0;
+    memset(ms58xxp->calibration, 0, sizeof(ms58xxp->calibration));
+#if MS58XX_USE_MUTUAL_EXCLUSION
+#if CH_USE_MUTEXES
+    chMtxInit(&ms58xxp->mutex);
+#else
+    chSemInit(&ms58xxp->semaphore, 1);
+#endif
+#endif /* MS58XX_USE_MUTUAL_EXCLUSION */
+}
+
+/**
+ * @brief   Configures and activates the FLASH peripheral.
+ *
+ * @param[in] ms58xxp   pointer to the @p MS58XXDriver object
+ * @param[in] config    pointer to the @p MS58XXConfig object.
+ *
+ * @api
+ */
+void ms58xxStart(MS58XXDriver* ms58xxp, const MS58XXConfig* configp)
+{
+    chDbgCheck((ms58xxp != NULL) && (configp != NULL), "ms58xxStart");
+    /* Verify device status. */
+    chDbgAssert((ms58xxp->state == MS58XX_STOP) || (ms58xxp->state == MS58XX_READY),
+            "ms58xxStart(), #1", "invalid state");
+
+    ms58xxp->configp = configp;
+
+    /* Reset device. */
+    {
+        const uint8_t txbuf[] = { MS58XX_COMMAND_RESET };
+
+        msg_t result;
+        result = i2cMasterTransmitTimeout(ms58xxp->configp->i2cp,
+                ms58xxp->configp->i2c_address >> 1,
+                txbuf,
+                sizeof(txbuf),
+                NULL,
+                0,
+                ms58xxp->configp->i2c_timeout);
+
+        if (result != RDY_OK)
+        {
+            ms58xxp->state = MS58XX_STOP;
+            return;
+        }
+
+        /* Sensor requires 2.8 ms to restart. */
+        chThdSleepMilliseconds(3);
+    }
+
+    /* Read calibration data. */
+    for (size_t i = 0; i < 8; ++i)
+    {
+        const uint8_t txbuf[] = { MS58XX_COMMAND_READ_PROM + (i << 1) };
+        uint8_t rxbuf[] = { 0x00, 0x00 };
+
+        msg_t result;
+        result = i2cMasterTransmitTimeout(ms58xxp->configp->i2cp,
+                ms58xxp->configp->i2c_address >> 1,
+                txbuf,
+                sizeof(txbuf),
+                rxbuf,
+                sizeof(rxbuf),
+                ms58xxp->configp->i2c_timeout);
+
+        if (result != RDY_OK)
+        {
+            ms58xxp->state = MS58XX_STOP;
+            return;
+        }
+
+        ms58xxp->calibration[i] = (rxbuf[0] << 8) | rxbuf[1];
+    }
+
+    /* Verify crc. */
+    {
+        uint16_t n_rem = 0x00;
+        uint16_t crc_read = ms58xxp->calibration[MS58XX_CAL_CRC];
+        ms58xxp->calibration[MS58XX_CAL_CRC] =
+                ms58xxp->calibration[MS58XX_CAL_CRC] & 0xff00;
+
+        for (size_t cnt = 0; cnt < 16; ++cnt)
+        {
+            if (cnt % 2 == 1)
+                n_rem ^= ms58xxp->calibration[cnt >> 1] & 0x00ff;
+            else
+                n_rem ^= ms58xxp->calibration[cnt >> 1] >> 8;
+
+            for (uint8_t n_bit = 8; n_bit > 0; --n_bit)
+            {
+                if (n_rem & 0x8000)
+                {
+                    n_rem = (n_rem << 1) ^ 0x3000;
+                }
+                else
+                {
+                    n_rem = n_rem << 1;
+                }
+            }
+        }
+        n_rem = (n_rem >> 12) & 0x000f;
+        ms58xxp->calibration[MS58XX_CAL_CRC] = crc_read;
+
+        if (n_rem != (ms58xxp->calibration[MS58XX_CAL_CRC] & 0x0f))
+        {
+            ms58xxp->state = MS58XX_STOP;
+            return;
+        }
+    }
+
+    ms58xxp->state = MS58XX_READY;
+}
+
+/**
+ * @brief   Disables the FLASH peripheral.
+ *
+ * @param[in] ms58xxp      pointer to the @p MS58XXDriver object
+ *
+ * @api
+ */
+void ms58xxStop(MS58XXDriver* ms58xxp)
+{
+    chDbgCheck(ms58xxp != NULL, "ms58xxStop");
+    /* Verify device status. */
+    chDbgAssert((ms58xxp->state == MS58XX_STOP) || (ms58xxp->state == MS58XX_READY),
+            "ms58xxStop(), #1", "invalid state");
+
+    ms58xxp->state = MS58XX_STOP;
+}
+
+/**
+ * @brief   Gains exclusive access to the flash device.
+ * @details This function tries to gain ownership to the flash device, if the
+ *          device is already being used then the invoking thread is queued.
+ * @pre     In order to use this function the option
+ *          @p MS58XX_USE_MUTUAL_EXCLUSION must be enabled.
+ *
+ * @param[in] ms58xxp      pointer to the @p MS58XXDriver object
+ *
+ * @api
+ */
+void ms58xxAcquireBus(MS58XXDriver* ms58xxp)
+{
+    chDbgCheck(ms58xxp != NULL, "ms58xxAcquireBus");
+
+#if MS58XX_USE_MUTUAL_EXCLUSION || defined(__DOXYGEN__)
+#if CH_USE_MUTEXES
+    chMtxLock(&ms58xxp->mutex);
+#elif CH_USE_SEMAPHORES
+    chSemWait(&ms58xxp->semaphore);
+#endif
+
+#if I2C_USE_MUTUAL_EXCLUSION
+    /* Acquire the underlying device as well. */
+    i2cAcquireBus(ms58xxp->config->i2cp);
+#endif /* I2C_USE_MUTUAL_EXCLUSION */
+#endif /* MS58XX_USE_MUTUAL_EXCLUSION */
+}
+
+/**
+ * @brief   Releases exclusive access to the flash device.
+ * @pre     In order to use this function the option
+ *          @p MS58XX_USE_MUTUAL_EXCLUSION must be enabled.
+ *
+ * @param[in] ms58xxp   pointer to the @p MS58XXDriver object
+ *
+ * @api
+ */
+void ms58xxReleaseBus(MS58XXDriver* ms58xxp)
+{
+    chDbgCheck(ms58xxp != NULL, "ms58xxReleaseBus");
+
+#if MS58XX_USE_MUTUAL_EXCLUSION || defined(__DOXYGEN__)
+#if CH_USE_MUTEXES
+    (void)ms58xxp;
+    chMtxUnlock();
+#elif CH_USE_SEMAPHORES
+    chSemSignal(&ms58xxp->semaphore);
+#endif
+
+#if I2C_USE_MUTUAL_EXCLUSION
+    /* Release the underlying device as well. */
+    i2cReleaseBus(ms58xxp->config->i2cp);
+#endif /* I2C_USE_MUTUAL_EXCLUSION */
+#endif /* MS58XX_USE_MUTUAL_EXCLUSION */
+}
+
+/**
+ * @brief   Starts a temperature conversion.
+ *
+ * @param[in] ms58xxp   pointer to the @p MS58XXDriver object
+ * @param[in] osr       oversampling
+ *
+ * @return              The operation status.
+ * @retval CH_SUCCESS   the operation succeeded.
+ * @retval CH_FAILED    the operation failed.
+ *
+ * @api
+ */
+bool_t ms58xxTemperatureStart(MS58XXDriver* ms58xxp, enum ms58xx_osr_e osr)
+{
+    chDbgCheck(ms58xxp != NULL, "ms58xxTemperatureStart");
+    /* Verify device status. */
+    chDbgAssert(ms58xxp->state == MS58XX_READY,
+            "ms58xxTemperatureStart(), #1", "invalid stateMS58XXDriver");
+
+    ms58xxp->state = MS58XX_ACTIVE;
+
+    /* Initiate temperature conversion. */
+    const uint8_t txbuf[] = { MS58XX_COMMAND_ACQUIRE_D2 + osr };
+
+    msg_t result;
+    result = i2cMasterTransmitTimeout(ms58xxp->configp->i2cp,
+            ms58xxp->configp->i2c_address >> 1,
+            txbuf,
+            sizeof(txbuf),
+            NULL,
+            0,
+            ms58xxp->configp->i2c_timeout);
+
+    if (result != RDY_OK)
+        return CH_FAILED;
+
+    return CH_SUCCESS;
+}
+
+/**
+ * @brief   Finishes a temperature conversion.
+ *
+ * @param[in] ms58xxp   pointer to the @p MS58XXDriver object
+ * @param[out] resultp  pointer to temperature result in degree C
+ *
+ * @return              The operation status.
+ * @retval CH_SUCCESS   the operation succeeded.
+ * @retval CH_FAILED    the operation failed.
+ *
+ * @api
+ */
+bool_t ms58xxTemperatureResult(MS58XXDriver* ms58xxp, float *resultp)
+{
+    chDbgCheck(ms58xxp != NULL, "ms58xxTemperatureResult");
+    /* Verify device status. */
+    chDbgAssert(ms58xxp->state == MS58XX_ACTIVE,
+            "ms58xxTemperatureResult(), #1", "invalid state");
+
+    /* Read result from chip. */
+    {
+        const uint8_t txbuf[] = { MS58XX_COMMAND_READ_ADC };
+        uint8_t rxbuf[] = { 0x00, 0x00, 0x00 };
+
+        msg_t result;
+        result = i2cMasterTransmitTimeout(ms58xxp->configp->i2cp,
+                ms58xxp->configp->i2c_address >> 1,
+                txbuf,
+                sizeof(txbuf),
+                rxbuf,
+                sizeof(rxbuf),
+                ms58xxp->configp->i2c_timeout);
+
+        if (result != RDY_OK)
+        {
+            /* Reset driver state. */
+            ms58xxp->state = MS58XX_READY;
+
+            return CH_FAILED;
+        }
+
+        ms58xxp->last_d2 = (rxbuf[0] << 16) | (rxbuf[1] << 8) | (rxbuf[2] << 0);
+    }
+
+    /* Reset driver state. */
+    ms58xxp->state = MS58XX_READY;
+
+    /* Calculate result. */
+    int32_t dT = (int32_t)ms58xxp->last_d2 -
+            ((int32_t)ms58xxp->calibration[MS58XX_CAL_5] << 8);
+
+    int32_t TEMP = 2000 + ((dT * ms58xxp->calibration[MS58XX_CAL_6]) >> 23);
+
+    /* Apply second order temperature compenstation. */
+    if (TEMP < 2000)
+    {
+        int32_t T2 = ((int64_t)3 * dT * dT) >> 33;
+        TEMP -= T2;
+    }
+    else
+    {
+        int32_t T2 = ((int64_t)7 * dT * dT) >> 37;
+        TEMP -= T2;
+    }
+
+    if (resultp != NULL)
+        *resultp = TEMP / 100.0f;
+
+    return CH_SUCCESS;
+}
+
+/**
+ * @brief   Starts a pressure conversion.
+ *
+ * @param[in] ms58xxp   pointer to the @p MS58XXDriver object
+ * @param[in] osr       oversampling
+ *
+ * @return              The operation status.
+ * @retval CH_SUCCESS   the operation succeeded.
+ * @retval CH_FAILED    the operation failed.
+ *
+ * @api
+ */
+bool_t ms58xxPressureStart(MS58XXDriver* ms58xxp, enum ms58xx_osr_e osr)
+{
+    chDbgCheck(ms58xxp != NULL, "ms58xxPressureStart");
+    /* Verify device status. */
+    chDbgAssert(ms58xxp->state == MS58XX_READY,
+            "ms58xxPressureStart(), #1", "invalid state");
+
+    ms58xxp->state = MS58XX_ACTIVE;
+
+    /* Initiate pressure conversion. */
+    const uint8_t txbuf[] = { MS58XX_COMMAND_ACQUIRE_D1 + osr };
+
+    msg_t result;
+    result = i2cMasterTransmitTimeout(ms58xxp->configp->i2cp,
+            ms58xxp->configp->i2c_address >> 1,
+            txbuf,
+            sizeof(txbuf),
+            NULL,
+            0,
+            ms58xxp->configp->i2c_timeout);
+
+    if (result != RDY_OK)
+        return CH_FAILED;
+
+    return CH_SUCCESS;
+}
+
+/**
+ * @brief   Finishes a pressure conversion.
+ *
+ * @param[in] ms58xxp   pointer to the @p MS58XXDriver object
+ * @param[out] resultp  pointer to pressure result in bar
+ *
+ * @return              The operation status.
+ * @retval CH_SUCCESS   the operation succeeded.
+ * @retval CH_FAILED    the operation failed.
+ *
+ * @api
+ */
+bool_t ms58xxPressureResult(MS58XXDriver* ms58xxp, float *resultp)
+{
+    chDbgCheck(ms58xxp != NULL, "ms58xxPressureResult");
+    /* Verify device status. */
+    chDbgAssert(ms58xxp->state == MS58XX_ACTIVE,
+            "ms58xxPressureResult(), #1", "invalid state");
+
+    /* Read result from chip. */
+    {
+        const uint8_t txbuf[] = { MS58XX_COMMAND_READ_ADC };
+        uint8_t rxbuf[] = { 0x00, 0x00, 0x00 };
+
+        msg_t result;
+        result = i2cMasterTransmitTimeout(ms58xxp->configp->i2cp,
+                ms58xxp->configp->i2c_address >> 1,
+                txbuf,
+                sizeof(txbuf),
+                rxbuf,
+                sizeof(rxbuf),
+                ms58xxp->configp->i2c_timeout);
+
+        if (result != RDY_OK)
+        {
+            /* Reset driver state. */
+            ms58xxp->state = MS58XX_READY;
+
+            return CH_FAILED;
+        }
+
+        ms58xxp->last_d1 = (rxbuf[0] << 16) | (rxbuf[1] << 8) | (rxbuf[2] << 0);
+    }
+
+    /* Reset driver state. */
+    ms58xxp->state = MS58XX_READY;
+
+    /* Calculate result. */
+    int32_t dT = (int32_t)ms58xxp->last_d2 -
+            (int32_t)ms58xxp->calibration[MS58XX_CAL_5] * 256;
+
+    int32_t TEMP = 2000 + dT * ms58xxp->calibration[MS58XX_CAL_6] / 8388608;
+
+    int64_t OFF = (int64_t)ms58xxp->calibration[MS58XX_CAL_2] * 65536 +
+            ((int64_t)ms58xxp->calibration[MS58XX_CAL_4] * dT) / 128;
+
+    int64_t SENS = (int64_t)ms58xxp->calibration[MS58XX_CAL_1] * 32768 +
+            ((int64_t)ms58xxp->calibration[MS58XX_CAL_3] * dT) / 256;
+
+    /* Apply second order temperature compenstation. */
+    if (TEMP < 2000)
+    {
+        int64_t OFF2 = 3 * (TEMP - 2000) * (TEMP - 2000) / 2;
+        int64_t SENS2 = 5 * (TEMP - 2000) * (TEMP - 2000) / 8;
+
+        if (TEMP < -1500)
+        {
+            OFF2 = OFF2 + 7 * (TEMP + 1500) * (TEMP + 1500);
+            SENS2 = SENS2 + 4 * (TEMP + 1500) * (TEMP + 1500);
+        }
+
+        OFF -= OFF2;
+        SENS -= SENS2;
+    }
+    else
+    {
+        int64_t OFF2 = 1 * (TEMP - 2000) * (TEMP - 2000) / 16;
+
+        OFF -= OFF2;
+    }
+
+    int32_t P = (ms58xxp->last_d1 * SENS / 2097152 - OFF) / 8192;
+
+    if (resultp != NULL)
+        *resultp = P / 10000.0f;
+
+    return CH_SUCCESS;
+}
+
+#endif /* HAL_USE_MS58XX */
+
+/** @} */
