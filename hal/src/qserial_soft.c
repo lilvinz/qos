c737667a9a426acbf624515dda6fb27bb82821c6
cba <carsten.bartels@quantec-networks.de>
2017-02-28 10:14:39 +0100

    hal/qserial_soft: fix null pointer access on driver stop

--- hal/src/qserial_soft.c
+++ hal/src/qserial_soft.c
@@ -410,7 +410,6 @@ void serialsoftStop(SerialSoftDriver *qsvip)
             "serialsoftStop(), #1",
             "invalid state");
     qsvip->state = SERIALSOFT_STOP;
-    qsvip->config = NULL;
 
 #if SERIALSOFT_USE_TRANSMITTER
     /* ToDo: Stop tx environment */
@@ -432,6 +431,8 @@ void serialsoftStop(SerialSoftDriver *qsvip)
 #if SERIALSOFT_USE_RECEIVER
     gptStop(qsvip->config->gptd);
 #endif
+
+    qsvip->config = NULL;
 }
 
 #endif /* HAL_USE_SERIAL_SOFT */
