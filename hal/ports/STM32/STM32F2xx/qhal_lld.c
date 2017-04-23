78edd0feba5fb03ba0978b210a090ec74f60a6e5
vke <vincent.kessler@quantec-networks.de>
2017-04-23 09:48:29 +0200

    unify all file license headers

--- hal/ports/STM32/STM32F2xx/qhal_lld.c
+++ hal/ports/STM32/STM32F2xx/qhal_lld.c
@@ -1,6 +1,4 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
-
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
@@ -13,7 +11,6 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
-
 /**
  * @file    STM32F2xx/qhal_lld.c
  * @brief   STM32F2xx HAL subsystem low level driver source.
