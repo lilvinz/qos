fccf669b0c785137d9e426e3f723a03bbba01310
vke <vincent.kessler@quantec-networks.de>
2017-06-19 23:53:39 +0200

    simulator/posix/st_lld.h: fix shadowing of global time variable

--- hal/ports/simulator/posix/st_lld.h
+++ hal/ports/simulator/posix/st_lld.h
@@ -120,13 +120,13 @@ static inline systime_t st_lld_get_counter(void) {
  * @note    Makes sure that no spurious alarms are triggered after
  *          this call.
  *
- * @param[in] time      the time to be set for the first alarm
+ * @param[in] alarm_time    the time to be set for the first alarm
  *
  * @notapi
  */
-static inline void st_lld_start_alarm(systime_t time) {
+static inline void st_lld_start_alarm(systime_t alarm_time) {
 
-  (void)time;
+  (void)alarm_time;
 }
 
 /**
@@ -141,13 +141,13 @@ static inline void st_lld_stop_alarm(void) {
 /**
  * @brief   Sets the alarm time.
  *
- * @param[in] time      the time to be set for the next alarm
+ * @param[in] alarm_time    the time to be set for the next alarm
  *
  * @notapi
  */
-static inline void st_lld_set_alarm(systime_t time) {
+static inline void st_lld_set_alarm(systime_t alarm_time) {
 
-  (void)time;
+  (void)alarm_time;
 }
 
 /**
