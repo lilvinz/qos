3d4cb95713ae4d3e1daca38984c8283b05b41f43
vke <vincent.kessler@quantec-networks.de>
2017-09-03 11:12:20 +0200

    STM32/STM32F0xx/library.mk: fix paths

--- hal/ports/STM32/STM32F0xx/library.mk
+++ hal/ports/STM32/STM32F0xx/library.mk
@@ -4,18 +4,18 @@ CHIBIOS_DIR ?= $(ROOT_DIR)/submodules/chibios
 CHIBIOS := $(CHIBIOS_DIR)
 CHIBIOS_CONTRIB := $(CHIBIOS)/community
 
-QHAL_STM32F1XX_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+QHAL_STM32F0XX_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-CSRC += $(wildcard $(QHAL_STM32F1XX_DIR)/*.c)
-CSRC += $(wildcard $(QHAL_STM32F1XX_DIR)/../LLD/FLASHv1/*.c)
-CSRC += $(wildcard $(QHAL_STM32F1XX_DIR)/../LLD/RTCv2/*.c)
+CSRC += $(wildcard $(QHAL_STM32F0XX_DIR)/*.c)
+CSRC += $(wildcard $(QHAL_STM32F0XX_DIR)/../LLD/FLASHv1/*.c)
+CSRC += $(wildcard $(QHAL_STM32F0XX_DIR)/../LLD/RTCv2/*.c)
 
-EXTRAINCDIRS += $(QHAL_STM32F1XX_DIR)
-EXTRAINCDIRS += $(QHAL_STM32F1XX_DIR)/../LLD/FLASHv1
-EXTRAINCDIRS += $(QHAL_STM32F1XX_DIR)/../LLD/RTCv2
+EXTRAINCDIRS += $(QHAL_STM32F0XX_DIR)
+EXTRAINCDIRS += $(QHAL_STM32F0XX_DIR)/../LLD/FLASHv1
+EXTRAINCDIRS += $(QHAL_STM32F0XX_DIR)/../LLD/RTCv2
 
 # ChibiOS
 include $(CHIBIOS_DIR)/os/common/ports/ARMCMx/compilers/GCC/mk/startup_stm32f0xx.mk
-include $(CHIBIOS_DIR)/os/community/hal/ports/STM32/STM32F0xx/platform.mk
+include $(CHIBIOS_DIR)/community/os/hal/ports/STM32/STM32F0xx/platform.mk
 
-include $(QHAL_STM32F4XX_DIR)/../library.mk
+include $(QHAL_STM32F0XX_DIR)/../library.mk
